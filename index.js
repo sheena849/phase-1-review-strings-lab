1eade84c7636c37ef32428815e88fc16a7243a1f
sheena849 <sheenamugo04@gmail.com>
2024-09-23 11:41:42 +0300

    Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const welcomeMessage = 'Welcome to Flatbook, ';
+//const welcomeMessage = currentUser;
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, GRACE HOPPER!`
+const shortGreeting = `Welcome, Grace!`
